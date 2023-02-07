e09a0de6c31649736480bba0ef7b2f350833a4ab
TasnadiRichard <112942145+TasnadiRichard@users.noreply.github.com>
2023-02-07 07:33:30 +0100

    Add files via upload

---
+++ byte/byte/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 143;
+
+            Console.WriteLine(x << 1);//10001111 (=143)<<1=100011110=286
+
+            Console.ReadKey();
+        }
+    }
+}
