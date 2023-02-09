89982b377c57197e4e8cf84d9d4c8b383527df4a
TasnadiRichard <112942145+TasnadiRichard@users.noreply.github.com>
2023-02-09 08:16:17 +0100

    Add files via upload

---
+++ 5.feladat/5.feladat/Program.cs
@@ -0,0 +1,16 @@
+﻿static void Main(string[] args)
+{
+    Console.WriteLine("A viz halmazallapotanak vizsgalata:");
+    Console.Write("Homerseklet: ");
+
+    double t = Convert.ToDouble(Console.ReadLine());
+
+    if (t > 0)
+    {
+        if (t >= 100) Console.WriteLine("Goz!");
+        else Console.WriteLine("Viz!");
+    }
+    else Console.WriteLine("Jeg!");
+
+    Console.ReadLine();
+}
---
+++ 6.feladat/6.feladat/Program.cs
@@ -0,0 +1,21 @@
+﻿static void Main(string[] args)
+{
+    Console.Write("Elso pont x kordinátája:");
+    int x1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Elso pont y kordinátája:");
+    int y1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Második pont x kordinátája:");
+    int x2 = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Második pont y kordinátája:");
+    int y2 = Convert.ToInt32(Console.ReadLine());
+
+    double tavolsag =
+    Math.Sqrt((x1 - x2) * (x1 - x2) + (y2 - y1) * (y2 - y1));
+
+    Console.Write("Távolság: {0}", tavolsag);
+
+    Console.ReadLine();
+}
