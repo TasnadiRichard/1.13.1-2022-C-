959b68ddf0228f2d65e5b10923e49917fd889279
TasnadiRichard <112942145+TasnadiRichard@users.noreply.github.com>
2023-02-20 07:47:29 +0100

    Add files via upload

---
+++ 2023.02.06 feladatok/2.feladat/16.feladat/16.feladat/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16.feladat
+{
+    
+    internal class Program
+
+        static double Szam(int osz)
+    {
+
+
+        return 0.0;
+    }
+
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Add meg hány oldal:");
+        double ere = Szam(int.Parse(Console.ReadLine()));
+    }
+    }
+}
